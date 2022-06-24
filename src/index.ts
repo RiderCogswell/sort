0c5f8e106e050a7bcb5cddbaa1868bf196c8e3d8
Ridercogswell <ridercogswell@gmail.com>
2022-06-23 20:01:23 -0700

    bubble sort

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log(2);
+class Sorter {
+  // collection: number[];
+
+  // constructor(collection: number[]) {
+  //   this.collection = collection;
+  // } 
+  // is the same as below because of public
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - 1; j++) {
+        if (this.collection[j] > this.collection[i]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j+1];
+          this.collection[j+1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0])
+sorter.sort();
+console.log(sorter.collection);
