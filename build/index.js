bb48e14346672a7d4b967487691d14d5d9c2b385
Ridercogswell <ridercogswell@gmail.com>
2022-07-11 00:57:34 -0700

    print logic

--- build/index.js
+++ build/index.js
@@ -1,12 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
-const CharactersCollection_1 = require("./CharactersCollection");
+const LinkedList_1 = require("./LinkedList");
 // const numbers = new NumbersCollection([10, 3, -5, 0])
 // const sorter = new Sorter(numbers)
 // sorter.sort();
 // console.log(numbers.data);
-const charCollect = new CharactersCollection_1.CharactersCollection('Xdjfuf');
-const sorter = new Sorter_1.Sorter(charCollect);
+// const charCollect = new CharactersCollection('Xdjfuf');
+// const sorter = new Sorter(charCollect)
+// sorter.sort()
+// console.log(charCollect.data);
+const linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(50);
+linkedList.add(30);
+linkedList.add(-80);
+const sorter = new Sorter_1.Sorter(linkedList);
 sorter.sort();
-console.log(charCollect.data);
+linkedList.print();
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
 import { Sorter } from './Sorter'
 import { NumbersCollection } from './NumberCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // const numbers = new NumbersCollection([10, 3, -5, 0])
 // const sorter = new Sorter(numbers)
 // sorter.sort();
 // console.log(numbers.data);
 
-const charCollect = new CharactersCollection('Xdjfuf');
-const sorter = new Sorter(charCollect)
-sorter.sort()
+// const charCollect = new CharactersCollection('Xdjfuf');
+// const sorter = new Sorter(charCollect)
+// sorter.sort()
 
-console.log(charCollect.data);
+// console.log(charCollect.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500)
+linkedList.add(50)
+linkedList.add(30)
+linkedList.add(-80)
+
+const sorter = new Sorter(linkedList)
+sorter.sort()
+linkedList.print();
