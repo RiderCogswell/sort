299a2e5be2a29c03a05de107657b9965043dc058
Ridercogswell <ridercogswell@gmail.com>
2022-07-10 18:05:57 -0700

    separate numbers collection logic

---
+++ src/NumberCollection.ts
@@ -0,0 +1,19 @@
+export class NumbersCollection {
+  constructor(public data: number[]) {}
+
+  // the get syntax here allows this to be called simply as .length 
+  // instead of having to perform, the function .length()
+  get length(): number {
+    return this.data.length
+  }
+
+  compare(leftIdx: number, rightIdx: number): boolean {
+    return this.data[leftIdx] > this.data[rightIdx];
+  }
+
+  swap(leftIdx: number, rightIdx: number): void {
+    const leftHand = this.data[leftIdx];
+    this.data[leftIdx] = this.data[rightIdx];
+    this.data[rightIdx] = leftHand;
+  }
+}
